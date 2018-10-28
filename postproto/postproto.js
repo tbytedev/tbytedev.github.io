20e514388959f46c86a242b262fb614d51b81b8a
Tbyte <44131636+tbytedev@users.noreply.github.com>
2018-10-28 19:47:25 +0100

    Ask permission dialog to use local storage

---
+++ postproto/postproto.js
@@ -0,0 +1,11 @@
+function ShowEULaw()
+{
+	div = document.getElementById("eulaw");
+	div.style.height = String(div.scrollHeight) + "px";
+}
+
+function HideEULaw()
+{
+	div = document.getElementById("eulaw");
+	div.style.height = "0px";
+}
